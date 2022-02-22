d7c92b1cefa3d7de39503d99f1ccde59379fba57
Anshika <anshikagupta45612@gmail.com>
2022-02-22 16:32:28 +0530

    added file in new branch

---
+++ two-sum.py
@@ -0,0 +1,6 @@
+class Solution:
+    def twoSum(self, nums, target):
+        for i1 in range(len(nums)-1):
+            for i2 in range(i1 + 1, len(nums)):
+                if nums[i1] + nums[i2] == target:
+                     return [i1, i2]   
